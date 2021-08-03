ca3d77d4bda7b408088c3cd3c97964674ec7a65a
nejat <nejatoz@gmail.com>
2021-08-03 00:56:09 -0400

    [new] added library and debug! docs

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,32 @@
 #![allow(clippy::module_name_repetitions)]
 #![allow(clippy::items_after_statements)]
 
+//! Utility library for working with ```cli``` output ergonomically.
+//!
+//! This is not a logging alternative, it's intended to produce output for end user consumption.
+//!
+//! It handles three levels of verbosity that can be set dynamically at runtime:
+//!
+//! * Quite - no output
+//! * Terse - used to provide minimal user output
+//! * Verbose - used to provide elaborated and/or additional user output
+//!
+//! Additionally, this library provides conditionally compiled debugging output intended to be
+//! used during application development.
+//!
+//! All other debugging and telemetry output is most likely better served with a logging library.
+//!
+//! # Roadmap
+//!
+//! * [x] ```debug!``` macro
+//! * [ ] ```Verbosity``` reporting level functionality
+//! * [ ] ```report!``` macro
+//!
+//! # Roadmap - ¿ Extra Credit ?
+//!
+//! * [ ] handle ```Option<T>``` expressions in addition to ```Result<T,E>```
+//!       conditional expressions
+
 mod debug;
 
 #[cfg(test)]
