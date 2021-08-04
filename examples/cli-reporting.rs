328f12b9691159d588592c7ebedaecba3be5e8f9
nejat <nejatoz@gmail.com>
2021-08-03 23:42:10 -0400

    [new] added simple examples demonstrating debug! and report!

---
+++ examples/cli-reporting.rs
@@ -0,0 +1,35 @@
+use std::env;
+
+use cli_toolbox::report;
+use cli_toolbox::Verbosity;
+use std::str::FromStr; // this is needed by the report! macro
+
+fn main() {
+    let level: Verbosity = env::args()
+        .map(|v| { Verbosity::from_str(&v) })
+        .filter(|v| v.is_ok())
+        .map(|v| { v.unwrap() })
+        .last().unwrap_or(Verbosity::Quite);
+
+    // this will never print
+    report! { "setting verbosity to {}", level }
+
+    // this can only be set once, level is quite until it's set
+    level.set_as_global();
+
+    report! { "\nset verbosity to {}", level }
+
+    // basic terse output
+    report! { "terse output example" }
+
+    // are getting bored of 42 yet!? ... Douglas Adam Rulez!!!
+    report! { VERBOSE "verbose formatted output example; {}", 42 }
+
+    // terse or verbose error message
+    report! {
+        ERR  "gratuitous error message\n";
+        ERRV "gratuitously more detailed error message";
+    }
+
+    report! { VERBOSE "so long and thanks for all the fish!\n" }
+}
