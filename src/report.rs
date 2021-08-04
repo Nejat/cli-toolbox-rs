b8a2f3c96e2d06c492453b61f3cb2bb1246d1af1
nejat <nejatoz@gmail.com>
2021-08-03 22:31:05 -0400

    [new] added report! docs

--- src/report.rs
+++ src/report.rs
@@ -1,4 +1,128 @@
-/// The `report!` macro provides progress and error reporting
+/// The `report!` macro provides progress and error reporting.
+///
+/// # Features
+///
+/// * you can report user output and error messages in two levels of detail,
+///   _i.e._ `Terse` _or_ `Verbose`
+///
+/// * the user can set the level of reporting they want to endure 🙃,
+///   _i.e._ `Quite`_,_ `Terse` _or_ `Verbose`
+///
+/// * all `Terse` output will also output if the level is set to `Verbose`
+///
+/// * you can also choose different output for `Terse` or `Verbose`
+///
+/// * you can format output with application state
+///
+/// # Terse Examples
+/// 
+/// These examples output only if [`Verbosity`] level is greater than `Quite`.
+///
+/// * _basic output to_ `stdout`
+/// 
+/// ```rust
+/// # use cli_toolbox::report;
+/// use cli_toolbox::Verbosity; // required by report!
+///
+/// report! { "some user output" }
+/// ```
+///
+/// * _formatted output to_ `stdout`
+///
+/// ```rust
+/// # use cli_toolbox::report;
+/// use cli_toolbox::Verbosity;
+///
+/// report! { "some formatted user output; {}", 42 }
+/// ```
+///
+/// * _basic output to_ `stderr`
+///
+/// ```rust
+/// # use cli_toolbox::report;
+/// use cli_toolbox::Verbosity;
+///
+/// report! { ERR "some error message" }
+/// ```
+///
+/// * _formatted output to_ `stderr`
+///
+/// ```rust
+/// # use cli_toolbox::report;
+/// use cli_toolbox::Verbosity;
+///
+/// report! { ERR "some formatted error message; {}", -42 }
+/// ```
+///
+/// # Verbose Examples
+///
+/// These examples output only if [`Verbosity`] level is greater than `Terse`.
+///
+/// * _basic output to_ `stdout`
+///
+/// ```rust
+/// # use cli_toolbox::report;
+/// use cli_toolbox::Verbosity;
+///
+/// report! { VERBOSE "some detailedd user output" }
+/// ```
+///
+/// * _formatted output to_ `stdout`
+///
+/// ```rust
+/// # use cli_toolbox::report;
+/// use cli_toolbox::Verbosity;
+///
+/// report! { VERBOSE "some formatted more detailed user output; {}", 42 }
+/// ```
+///
+/// * _basic output to_ `stderr`
+///
+/// ```rust
+/// # use cli_toolbox::report;
+/// use cli_toolbox::Verbosity;
+///
+/// report! { ERRV "some detailedd error message" }
+/// ```
+///
+/// * _formatted output to_ `stderr`
+///
+/// ```rust
+/// # use cli_toolbox::report;
+/// use cli_toolbox::Verbosity;
+///
+/// report! { ERRV "some formatted more detailed error message; {}", -42 }
+/// ```
+///
+/// # Terse or Verbose Examples
+///
+/// These examples conditionally output if [`Verbosity`] level is greater than `Quite`.
+///
+/// * _output to_ `stdout`
+///
+/// ```rust
+/// # use cli_toolbox::report;
+/// use cli_toolbox::Verbosity;
+///
+/// // the following will output only one of the two messages, either branch accepts formatting
+/// report! {
+///     TERSE   "some user output";
+///     VERBOSE "some formatted more detailed user output; {}", 42;
+/// }
+/// ```
+///
+/// * _output to_ `stderr`
+///
+/// ```rust
+/// # use cli_toolbox::report;
+/// use cli_toolbox::Verbosity;
+///
+/// // the following will output only one of the two messages, either branch accepts formatting
+/// report! {
+///     ERR  "some error message";
+///     ERRV "some formatted more detailed error message; {}", -42;
+/// }
+/// ```
 ///
 /// [`Verbosity`]: crate::verbosity::Verbosity
 #[macro_export]
