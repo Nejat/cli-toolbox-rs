8cefb8757e255c40ca6a0ec2a6172da52b3be22b
nejat <nejatoz@gmail.com>
2021-08-05 19:27:50 -0400

    [new] added eval! docs

--- src/eval.rs
+++ src/eval.rs
@@ -1,4 +1,92 @@
 /// The `eval!` macro provides conditional code evaluation according to [`Verbosity`] level.
+///
+/// # Features
+///
+/// * you can evaluate code according to level of verbosity,
+///   _i.e._ `Quite`, `Terse` _or_ `Verbose`
+///
+/// * all `Terse` evaluations will also evaluate if the level is set to `Verbose`
+///
+/// * you can choose unique evaluations for `Terse` or `Verbose` individually
+///
+/// # Basic Examples
+///
+/// * _evaluates if [`Verbosity`] is_ `Quite`
+///
+/// ```rust
+/// # use cli_toolbox::eval;
+/// eval! { QUITE foo() }
+/// # fn foo() { /* ... */ }
+/// ```
+///
+/// * _evaluates if [`Verbosity`] is_ `Terse` or `Verbose`
+///
+/// ```rust
+/// # use cli_toolbox::eval;
+/// eval! { TERSE foo() }
+/// # fn foo() { /* ... */ }
+/// ```
+///
+/// * _evaluates if [`Verbosity`] is_ `Verbose`
+///
+/// ```rust
+/// # use cli_toolbox::eval;
+/// eval! { VERBOSE foo() }
+/// # fn foo() { /* ... */ }
+/// ```
+///
+/// # Terse or Verbose Examples
+///
+/// These examples evaluate conditionally based on [`Verbosity`] level.
+///
+/// * _terse or verbose code blocks_
+///
+/// ```rust
+/// # use cli_toolbox::eval;
+/// eval! {
+///     TERSE   { foo(); }
+///     VERBOSE { junk(); }
+/// }
+/// # fn foo() { /* ... */ }
+/// # fn junk() { /* ... */ }
+/// ```
+///
+/// * _terse or verbose expressions_
+///
+/// ```rust
+/// # use cli_toolbox::eval;
+/// eval! {
+///     TERSE   foo(),
+///     VERBOSE junk()
+/// }
+/// # fn foo() { /* ... */ }
+/// # fn junk() { /* ... */ }
+/// ```
+///
+/// * _terse expression or verbose block_
+///
+/// ```rust
+/// # use cli_toolbox::eval;
+/// eval! {
+///     TERSE   foo(),
+///     VERBOSE { junk(); }
+/// }
+/// # fn foo() { /* ... */ }
+/// # fn junk() { /* ... */ }
+/// ```
+///
+/// * _terse block or verbose expression_
+///
+/// ```rust
+/// # use cli_toolbox::eval;
+/// eval! {
+///     TERSE   { foo(); }
+///     VERBOSE junk()
+/// }
+/// # fn foo() { /* ... */ }
+/// # fn junk() { /* ... */ }
+/// ```
+///
 /// [`Verbosity`]: crate::verbosity::Verbosity
 #[macro_export]
 macro_rules! eval {
