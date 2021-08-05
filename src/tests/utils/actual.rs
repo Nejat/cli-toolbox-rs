c9b54b81f2d5d3cd9ead51c0885b16071409152c
nejat <nejatoz@gmail.com>
2021-08-04 21:31:23 -0400

    [refactor] new and updated testing utility macros
    * add release mode to actual! macro
    * switched expect! macro args to expr

--- src/tests/utils/actual.rs
+++ src/tests/utils/actual.rs
@@ -2,6 +2,7 @@
 #[doc(hidden)]
 #[macro_export]
 macro_rules! actual {
+    // debug uninitialized actual variable
     ($var:ident: $typ:ty) => {
         cfg_if::cfg_if! {
             if #[cfg(not(debug_assertions))] {
@@ -11,12 +12,33 @@ macro_rules! actual {
             }
         }
     };
+    // release uninitialized actual variable
+    (@rls $var:ident: $typ:ty) => {
+        cfg_if::cfg_if! {
+            if #[cfg(not(debug_assertions))] {
+                let $var: $typ;
+            } else {
+                let $var: $typ = Default::default();
+            }
+        }
+    };
+    // debug actual variable
     ($var:ident = $exp:expr) => {
         cfg_if::cfg_if! {
             if #[cfg(not(debug_assertions))] {
                 let $var = $exp;
             } else {
                 let mut $var = $exp;
+            }
+        }
+    };
+    // release actual variable
+    (@rls $var:ident = $exp:expr) => {
+        cfg_if::cfg_if! {
+            if #[cfg(not(debug_assertions))] {
+                let mut $var = $exp;
+            } else {
+                let $var = $exp;
             }
         }
     };
