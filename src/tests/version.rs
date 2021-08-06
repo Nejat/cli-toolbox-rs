f468572010d8951fd573eee135ac2d4457efa93d
nejat <nejatoz@gmail.com>
2021-08-05 20:37:41 -0400

    [update] release version v0.5.0

--- src/tests/version.rs
+++ src/tests/version.rs
@@ -4,6 +4,16 @@ fn readme_usage_version() {
 }
 
 #[test]
+fn readme_docs_link_version() {
+    version_sync::assert_contains_regex!("README.md", "/cli-toolbox/{version}/cli_toolbox/");
+}
+
+#[test]
+fn readme_examples_link_version() {
+    version_sync::assert_contains_regex!("README.md", "/v{version}/examples");
+}
+
+#[test]
 fn html_root_url() {
     version_sync::assert_html_root_url_updated!("src/lib.rs");
 }
