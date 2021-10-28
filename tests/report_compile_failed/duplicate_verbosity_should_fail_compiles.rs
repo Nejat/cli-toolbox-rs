361cb1c383ed450764b99e3ce625090f57dac615
nejat <nejatoz@gmail.com>
2021-10-28 16:48:49 -0400

    [update]  added additional duplicate verbosity unit test cases

--- tests/report_compile_failed/duplicate_verbosity_should_fail_compiles.rs
+++ tests/report_compile_failed/duplicate_verbosity_should_fail_compiles.rs
@@ -12,8 +12,28 @@ fn main() {
     }
 
     report! {
+        @terse "error terse message: {}", 42;
+        @err-terse "error terse message: {}", -42
+    }
+
+    report! {
+        @err-terse "error terse message: {}", -42;
+        @terse "error terse message: {}", 42
+    }
+
+    report! {
         @err-terse "error terse message: {}", -42;
         @err-terse "error terse message: {}", -42
+    }
+
+    report! {
+        @verbose "error verbose message: {}", 42;
+        @err-verbose "error verbose message: {}", -42
+    }
+
+    report! {
+        @err-verbose "error verbose message: {}", -42;
+        @verbose "error verbose message: {}", 42
     }
 
     report! {
